49f4de6d0180af54907c58355cd4c0dcb2949598
kokoisan <tpptpptpp57@gmail.com>
2024-12-09 14:06:00 +0900

    test

--- hyprpaper_loader.sh
+++ hyprpaper_loader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-directory="/home/tpp57/Pictures/Wallpapers/"
+directory="/home/tpp57/Pictures/Wallpapers"
 
 # ファイルをランダムに配列に読み込む
 readarray -t random_files < <(find "$directory" -maxdepth 1 -type f -printf "%f\n" | shuf)
@@ -11,8 +11,7 @@ length=${#random_files[@]}
 while true; do
   for ((i=0; i<length; i++)); do
     hyprctl hyprpaper preload "${directory}/${random_files[i]}"
-    hyprctl hyprpaper wallpaper "DP-1,${directory}/${random_files[i]}"
-    hyprctl hyprpaper wallpaper "HDMI-A-1,${directory}/${random_files[i]}"
+    hyprctl hyprpaper wallpaper "eDP-1,${directory}/${random_files[i]}"
     sed -i "6c path=${directory}/${random_files[i]}" /home/tpp57/.config/hypr/hyprlock.conf
     matugen image "${directory}/${random_files[i]}"
     sleep 1s
